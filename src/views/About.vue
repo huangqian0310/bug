0c1b698b8eb417047adead67ecee023443bb64db
huangqian <huangqian@fzxs.com.cn>
2021-09-28 14:56:01 +0800

    init

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  About页面
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+
+  setup() {
+    console.log(123122)
+  },
+});
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <a-layout>
+    <a-layout-header>Header</a-layout-header>
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout-content>
+        <router-view v-slot="{ Component, route }">
+          <keep-alive>
+            <component :is="Component" :key="route.fullPath" />
+          </keep-alive>
+        </router-view>
+      </a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+  
+</template>
